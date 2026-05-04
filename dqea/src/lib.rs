6f89a7fc4270a77f78789b82cb6036b28d8c96f1
SmartTree-zq1997 <drqiangzhu@gmail.com>
2026-05-04 21:48:32 +0800

    Update lib.rs

--- dqea/src/lib.rs
+++ dqea/src/lib.rs
@@ -1,1 +1,26 @@
 
+pub mod distributed_ecdsa;
+pub mod interpolation;
+pub mod triple;
+
+use ark_bls12_381::{Bls12_381, Fr, G1Affine, G1Projective};
+use ark_ec::{pairing::Pairing,  CurveGroup, PrimeGroup};
+use ark_ff::{Field,  UniformRand, Zero};
+use ark_poly::{
+    univariate::DensePolynomial
+};
+use ark_std::rand::{ RngCore};
+use sha2::{Digest, Sha256,};
+
+use num_bigint::BigUint;
+type Poly = DensePolynomial<Fr>;
+type GT = <Bls12_381 as Pairing>::TargetField;
+
+use distributed_ecdsa::{Commitment, Share, Srs, Witness, commit, create_witness, open, poly_from_coeffs, setup, verify_aggregated};
+use interpolation::{ith_lagrange_coefficient};
+use triple::{AdditiveShares, BeaverTripleShares, beaver_multiply_n_n, gen_beaver_triple_n_n, share_secret_n_n};
+use std::time::{Duration, Instant};
+
+use rayon::{prelude::*};
+use rand::{RngExt, SeedableRng};
+use rand::rngs::StdRng;
