e8f1036accbd622bf2e1e8b6d62681ed100442b2
SmartTree-zq1997 <drqiangzhu@gmail.com>
2026-05-06 09:37:57 +0800

    Update lib.rs

--- dqea/src/lib.rs
+++ dqea/src/lib.rs
@@ -37,3 +37,152 @@ pub fn dqea_setup<R: RngCore>(
 
     srs
 }
+
+pub fn dqea_thresshare_one_phase(
+    total_num: usize,
+    thres_num: usize,
+    srs: Srs,
+    rho: Fr,
+) -> (
+    Vec<Fr>,
+    Vec<Commitment>,
+    Vec<G1Projective>,
+    Vec<DensePolynomial<Fr>>,
+) {
+    let mut vecs: Vec<_> =
+        (0..total_num)
+            .into_par_iter()
+            .enumerate()
+            .map(
+                |(idx, _)| {
+                    let mut rng =
+                        StdRng::seed_from_u64(
+                            idx as u64,
+                        );
+
+                    let coeffs: Vec<_> =
+                        (0..=thres_num)
+                            .map(
+                                |_| {
+                                    rng.random_range(
+                                        10011..255451,
+                                    )
+                                },
+                            )
+                            .collect();
+
+                    poly_from_coeffs(
+                        &coeffs,
+                    )
+                },
+            )
+            .collect();
+
+    let x_i_vector =
+        vecs
+            .par_iter()
+            .map(
+                |vec| {
+                    vec[0]
+                },
+            )
+            .collect::<Vec<_>>();
+
+    let comm_i_vector =
+        vecs
+            .par_iter()
+            .map(
+                |poly| {
+                    commit(
+                        &srs,
+                        &poly,
+                    )
+                    .unwrap()
+                },
+            )
+            .collect::<Vec<_>>();
+
+    let y_i_vector =
+        vecs
+            .par_iter()
+            .map(
+                |x| {
+                    G1Projective::generator()
+                        * x[0]
+                },
+            )
+            .collect::<Vec<_>>();
+
+    let results: Vec<_> =
+        (0..total_num)
+            .into_par_iter()
+            .map(
+                |j| {
+                    let value =
+                        open(
+                            &vecs[1],
+                            Fr::from(
+                                j as u32,
+                            ),
+                        );
+
+                    let fy_small_val =
+                        G1Projective::generator()
+                            * value.0;
+
+                    let w_val =
+                        create_witness(
+                            &srs,
+                            &vecs[1],
+                            2,
+                            Fr::from(
+                                j as u32,
+                            ),
+                            rho,
+                        )
+                        .unwrap();
+
+                    (
+                        value.clone(),
+                        fy_small_val,
+                        w_val,
+                    )
+                },
+            )
+            .collect();
+
+    let (
+        mut fy_big,
+        mut fy_small,
+        mut w,
+    ) = (
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+    );
+
+    for (
+        value,
+        fy_small_val,
+        w_val,
+    ) in results {
+        fy_big.push(
+            value,
+        );
+
+        fy_small.push(
+            fy_small_val,
+        );
+
+        w.push(
+            w_val,
+        );
+    }
+
+    (
+        x_i_vector,
+        comm_i_vector,
+        y_i_vector,
+        vecs,
+    )
+}
