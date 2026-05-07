d9b867e13c8d09f8fc6f089f7faa9113110686dd
SmartTree-zq1997 <drqiangzhu@gmail.com>
2026-05-07 13:10:43 +0800

    Update main.rs

--- dqea/src/main.rs
+++ dqea/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-}
+use ark_bls12_381::{Fr};
+use ark_ff::{ Zero};
+
+use std::fs;
+
+use std::time::{Duration, Instant};
+
+use rayon::{prelude::*};
+
+use dqea::{dqea_setup,dqea_thresshare_one_phase,dqea_quote_phase, dqea_thresshare_two_phase,dqea_compute_sharesandwitness,dqea_verify,dqea_prequote_phase,get_nizk_type2,verify_nizk_type2};
+use dqea::triple::gen_beaver_triple_n_n;
+use dqea::interpolation::{ith_lagrange_coefficient};
+
+
+const THRESHORD: usize = 100;
+const TOTAL_NUMBER: usize = 105;
+const HEADER_LEN: usize = 48;
+const REPORT_BODY_LEN: usize = 384;
+const HEADER_REPORT_BODY_LEN: usize = HEADER_LEN + REPORT_BODY_LEN;
